d5e9344fb70b8b9a90f3f9b3baed2f590fd62d21
AliAngelinaaa <nanzum1140@gmail.com>
2024-03-21 21:10:40 -0400

    footer editing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import '../css/footer.css';
+import logo from "../images/logo.png";
 
 function Footer() {
   return (
-    <div>
-        <div class="text">
+    <div className="footer">
+      <img className="logo" src={logo} alt="WIT Unite logo. it is a < with a red heart and > followed by Women in Tech" />
+
+      <div className="text">
         <span>Created By <a href="#">WIT Unite</a> | &#169; 2024 All Rights Reserved</span>
       </div>
     </div>
